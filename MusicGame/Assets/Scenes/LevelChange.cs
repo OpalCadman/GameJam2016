3c79f0e999e7c2bc0134096a8229e723398d7837
OpalCadman <little_opal@hotmail.co.uk>
2016-04-19 17:43:04 +0100

    This has all the menu stuffs

---
+++ MusicGame/Assets/Scenes/LevelChange.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class LevelChange : MonoBehaviour, IPointerEnterHandler, ISelectHandler, IDeselectHandler  {
+
+	public bool isSelected = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isSelected) {
+			if (Input.GetAxis ("LeftTrigger") > 0) 
+			{
+				Application.LoadLevel (1);
+			}
+		}
+	}
+
+	public void OnPointerEnter(PointerEventData eventdata)
+	{
+	}
+	public void OnSelect(BaseEventData eventdata)
+	{
+		isSelected = true;
+
+	}
+
+	public void OnDeselect(BaseEventData eventdata)
+	{
+		isSelected = false;
+	}
+}
